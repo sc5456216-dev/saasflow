64bf9958bae6d9f932302f463e19636fbc441bb5
sameer <sc5456216@gmail.com>
2026-08-20 07:45:04 +0545

    Complete password reset functionality
    - Added password reset complete template
    - Added password reset confirm template
    - Added password reset done template
    - Updated password reset HTML template
    - Updated URL configuration for password reset routes
    - Fixed password reset flow

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 from django.views.generic import RedirectView
 
 urlpatterns = [
@@ -16,6 +17,30 @@ urlpatterns = [
     
     # Redirect accounts/login to our login page
     path('accounts/login/', RedirectView.as_view(url='/login/', permanent=True)),
+    
+    # Password Reset URLs
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='registration/password_reset.html',
+             email_template_name='registration/password_reset_email.html',
+             subject_template_name='registration/password_reset_subject.txt'
+         ),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='registration/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='registration/password_reset_confirm.html'
+         ),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='registration/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
